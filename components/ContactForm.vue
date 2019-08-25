a349c7de9ca3b9a0f49367d66273414f6413f373
Mark Noonan <markthomasnoonan@gmail.com>
2019-08-25 17:27:44 -0400

    update contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,41 +1,36 @@
 <template>
-  <div v-editable="blok" class="column feature">
+  <form v-editable="blok" class="container mx-auto max-w-lg" name="contact" method="POST" data-netlify="true">
     <label class="block">
       <span class="text-gray-700">Name</span>
-      <input class="form-input mt-1 block w-full" placeholder="Jane Doe">
+      <input class="form-input mt-1 block w-full" name="name" placeholder="Jane Doe">
+    </label>
+    <label class="block">
+      <span class="text-gray-700">Email</span>
+      <input class="form-input mt-1 block w-full" type="email" name="email" placeholder="you@domain.com">
     </label>
 
     <div class="mt-4">
-      <span class="text-gray-700">Account Type</span>
+      <span class="text-gray-700">I want to:</span>
       <div class="mt-2">
-        <label class="inline-flex items-center">
-          <input type="radio" class="form-radio" name="accountType" value="personal">
-          <span class="ml-2">Personal</span>
+        <label class="inline-flex items-center ml-6">
+          <input type="radio" class="form-radio" name="accountType" value="suggest-event">
+          <span class="ml-2">Suggest a new event or place</span>
         </label>
         <label class="inline-flex items-center ml-6">
-          <input type="radio" class="form-radio" name="accountType" value="busines">
-          <span class="ml-2">Business</span>
+          <input type="radio" class="form-radio" name="accountType" value="general-comment">
+          <span class="ml-2">Make a general comment or give feedback on the site</span>
         </label>
       </div>
     </div>
-
-    <label class="block mt-4">
-      <span class="text-gray-700">Requested Limit</span>
-      <select class="form-select mt-1 block w-full">
-        <option>$1,000</option>
-        <option>$5,000</option>
-        <option>$10,000</option>
-        <option>$25,000</option>
-      </select>
+    <label class="block">
+      <span class="text-gray-700">Message</span>
+      <textarea rows="7" class="form-input mt-1 block w-full" name="message" />
     </label>
 
     <div class="flex mt-6">
-      <label class="flex items-center">
-        <input type="checkbox" class="form-checkbox">
-        <span class="ml-2">I agree to the <span class="underline">privacy policy</span></span>
-      </label>
+      <button type="submit">Submit</button>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
