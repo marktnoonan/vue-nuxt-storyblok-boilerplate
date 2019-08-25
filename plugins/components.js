35ec3592c0f52608a80ea4f75fdbda3f525ccbf2
Mark Noonan <markthomasnoonan@gmail.com>
2019-08-25 18:15:49 -0400

    add aaMap to component list

--- plugins/components.js
+++ plugins/components.js
@@ -4,9 +4,11 @@ import Teaser from '~/components/Teaser.vue'
 import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
 import ContactForm from '~/components/ContactForm'
+import aaMap from '~/components/Map'
 
 Vue.component('blok-page', Page)
 Vue.component('blok-teaser', Teaser)
 Vue.component('blok-grid', Grid)
 Vue.component('blok-feature', Feature)
 Vue.component('blok-contact-form', ContactForm)
+Vue.component('blok-map', aaMap)
