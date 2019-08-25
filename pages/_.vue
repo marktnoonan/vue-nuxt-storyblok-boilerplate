ff70dc0af7113966130447a82605ac2cf9801842
Mark Noonan <markthomasnoonan@gmail.com>
2019-08-25 18:04:44 -0400

    remove download button

--- pages/_.vue
+++ pages/_.vue
@@ -18,10 +18,6 @@
           <nuxt-link to="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             Contact
           </nuxt-link>
-
-        </div>
-        <div>
-          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
         </div>
       </div>
     </nav>
