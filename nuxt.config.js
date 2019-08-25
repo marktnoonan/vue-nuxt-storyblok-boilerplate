e8cd057352666c49937f974844a4d8e452093528
Mark Noonan <markthomasnoonan@gmail.com>
2019-08-25 17:15:19 -0400

    update generate command

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+import axios from 'axios';
 
 module.exports = {
   mode: 'universal',
@@ -55,7 +56,36 @@ module.exports = {
   router: {
     middleware: 'setCacheVersion'
   },
-
+  generate: {
+    routes: function (callback) {
+      const token = `pOF8tr4pklJritotx7T2rQtt`
+      const version = 'published'
+      let cache_version = 0
+  
+      
+       // other routes that are not in Storyblok with their slug.
+      let routes = ['/'] // adds / directly
+  
+       // Load space and receive latest cache version key to improve performance
+      axios.get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${token}`).then((space_res) => {
+  
+         // timestamp of latest publish
+        cache_version = space_res.data.space.version
+  
+         // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
+        axios.get(`https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&cv=${cache_version}`).then((res) => {
+          Object.keys(res.data.links).forEach((key) => {
+            if (res.data.links[key].slug != 'home') {
+              routes.push('/' + res.data.links[key].slug)
+            }
+          })
+  
+          callback(null, routes)
+        })
+      }) 
+    }
+  },
+  
   /*
   ** Build configuration
   */
