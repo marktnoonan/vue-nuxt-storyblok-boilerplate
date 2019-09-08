35ad9430590b1879bfcb3f7f1796c15bbffc47ee
Mark Noonan <markthomasnoonan@gmail.com>
2019-09-08 17:58:01 -0400

    comment out links

--- pages/_.vue
+++ pages/_.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-          <nuxt-link to="/about" class="block mt-4 lg:inline-block lg:mt-0 text-green-100 hover:text-white mr-4 text-lg">
+          <!-- <nuxt-link to="/about" class="block mt-4 lg:inline-block lg:mt-0 text-green-100 hover:text-white mr-4 text-lg">
             About
           </nuxt-link>
           <nuxt-link to="/events" class="block mt-4 lg:inline-block lg:mt-0 text-green-100 hover:text-white mr-4 text-lg">
@@ -14,7 +14,7 @@
           </nuxt-link>
           <nuxt-link to="/places" class="block mt-4 lg:inline-block lg:mt-0 text-green-100 hover:text-white mr-4 text-lg">
             Places
-          </nuxt-link>
+          </nuxt-link> -->
           <nuxt-link to="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-green-100 hover:text-white mr-4 text-lg">
             Contact
           </nuxt-link>
