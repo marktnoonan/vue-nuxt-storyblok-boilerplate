2b738c9197f9f6d6368e47f7e2e2e1f28559900f
Mark Noonan <markthomasnoonan@gmail.com>
2019-09-22 21:18:04 -0400

    padding on feature

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-editable="blok" class="column feature">
+  <div v-editable="blok" class="column feature p-2">
     <div class="title">{{ blok.name }}</div>
     <div class="text text-base leading-tight my-6" v-html="blok.text"></div>
   </div>
