2af4ab397103603da4e78ab3258bc4ddef47374c
Mark Noonan <markthomasnoonan@gmail.com>
2019-09-22 21:42:43 -0400

    center test on feature titles

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-editable="blok" class="column feature p-2">
-    <div class="title">{{ blok.name }}</div>
+    <div class="title text-center">{{ blok.name }}</div>
     <div class="text text-base leading-tight my-6" v-html="blok.text"></div>
   </div>
 </template>
