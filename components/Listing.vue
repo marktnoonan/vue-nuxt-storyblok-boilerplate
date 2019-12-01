1f32778c67112d7c1a386daf65eab6794291ff2d
Mark Noonan <markthomasnoonan@gmail.com>
2019-11-30 19:41:30 -0500

    add skeleton listing component

---
+++ components/Listing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-editable="blok" class="column feature p-2">
+    <div class="title text-center">{{ blok.name }}</div>
+    <div class="title text-center">{{ blok.category }}</div>
+    <div class="title text-center">{{ blok.banner }}</div>
+    <div class="title text-center">{{ blok.memberStatus }}</div>
+    <div class="title text-center">{{ blok.eventDetails }}</div>
+    <div class="title text-center">{{ blok.when }}</div>
+    <div class="title text-center">{{ blok.cost }}</div>
+    <div class="title text-center">{{ blok.age }}</div>
+    <div class="title text-center">{{ blok.specialNotes }}</div>
+    <div class="title text-center">{{ blok.fullAddress }}</div>
+    <div class="title text-center">{{ blok.onlineInfo }}</div>
+    <div class="title text-center">{{ blok.phone }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['blok']
+}
+</script>
