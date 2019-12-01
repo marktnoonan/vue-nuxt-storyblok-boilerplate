9b38110987c7cac4d78d1c4ca24f0442e1cd5c27
Mark Noonan <markthomasnoonan@gmail.com>
2019-11-30 20:11:59 -0500

    add listing component to components js and populate

--- components/Listing.vue
+++ components/Listing.vue
@@ -1,17 +1,59 @@
 <template>
-  <div v-editable="blok" class="column feature p-2">
-    <div class="title text-center">{{ blok.name }}</div>
-    <div class="title text-center">{{ blok.category }}</div>
-    <div class="title text-center">{{ blok.banner }}</div>
-    <div class="title text-center">{{ blok.memberStatus }}</div>
-    <div class="title text-center">{{ blok.eventDetails }}</div>
-    <div class="title text-center">{{ blok.when }}</div>
-    <div class="title text-center">{{ blok.cost }}</div>
-    <div class="title text-center">{{ blok.age }}</div>
-    <div class="title text-center">{{ blok.specialNotes }}</div>
-    <div class="title text-center">{{ blok.fullAddress }}</div>
-    <div class="title text-center">{{ blok.onlineInfo }}</div>
-    <div class="title text-center">{{ blok.phone }}</div>
+  <div v-editable="blok" class="column p-2">
+    <ul class="results-card leading-relaxed" :class="'cat__' + blok.category">
+    <li>
+      <div class="text-xl font-semibold pb-1 text-center">{{blok.name}}</div>
+    </li>
+    <li v-if="blok.category || blok.banner">
+       <span>{{blok.category}}</span>
+     
+      <span v-if="blok.category && blok.banner">-</span>
+      {{blok.banner}}
+      <span
+        v-if="blok.memberStatus.toLowerCase() == 'y'"
+        class="bg-red-500 text-white text-xs font-bold py-1 px-2"
+      >Member Only</span>
+    </li>
+    <li v-if="blok.eventDetails">
+      <div class="label">Details</div>
+      {{blok.eventDetails}}
+    </li>
+    <li v-if="blok.when">
+      <div class="label">When?</div>
+      {{blok.when}}
+    </li>
+    <li v-if="blok.cost">
+      <div class="label">Cost</div>
+      {{blok.cost}}
+    </li>
+    <li v-if="blok.age">
+      <div class="label">Age</div>
+      {{blok.age}}
+    </li>
+    <li v-if="blok.specialNotes">
+      <div class="label">Special Notes</div>
+      {{blok.specialNotes}}
+    </li>
+    <li v-if="blok.fullAddress">
+      <div class="label">Location</div>
+      <a
+        class="font-thin"
+        :href="'https://www.google.com/maps/place/' + blok.fullAddress + ' Ireland'"
+        target="_blank"
+      >{{blok.fullAddress}}</a>
+    </li>
+    <li v-if="blok.onlineInfo">
+      <div class="label">Online</div>
+      <a
+        :href="blok.onlineInfo"
+        class="text-sm font-thin break-all"
+      >{{blok.onlineInfo}}</a>
+    </li>
+    <li v-if="blok.phone">
+      <div class="label">Tel</div>
+      <a class="font-thin" :href="'tel:+'+blok.phone">{{blok.phone}}</a>
+    </li>
+  </ul>
     <div>{{blok.review}}</div>
   </div>
 </template>
--- plugins/components.js
+++ plugins/components.js
@@ -5,6 +5,7 @@ import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
 import ContactForm from '~/components/ContactForm'
 import Search from '~/components/Search'
+import Listing from '~/components/Listing'
 import aaMap from '~/components/Map'
 import VueFuse from "vue-fuse";
 
@@ -17,4 +18,5 @@ Vue.component('blok-feature', Feature)
 Vue.component('blok-contact-form', ContactForm)
 Vue.component('blok-map', aaMap)
 Vue.component('blok-search', Search)
+Vue.component('blok-listing', Listing)
 
